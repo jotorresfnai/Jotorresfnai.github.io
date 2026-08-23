1edc3362bbd0fa03642d6a993a21613982d0a513
Unreals <psageguedes@gmail.com>
2026-08-23 07:47:54 +0100

    fix: tornar sitemap XML explicitamente declarativo e legível

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -33,7 +33,7 @@ const xmlEscape = value => value
   .replace(/\"/g, "&quot;")
   .replace(/'/g, "&apos;");
 
-const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${uniqueUrls.map(url => `  <url><loc>${xmlEscape(url)}</loc></url>`).join("\n")}\n</urlset>\n`;
+const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${uniqueUrls.map(url => `  <url>\n    <loc>${xmlEscape(url)}</loc>\n  </url>`).join("\n")}\n</urlset>\n`;
 
 fs.writeFileSync("sitemap.xml", xml, "utf8");
 console.log(`Sitemap gerado com ${uniqueUrls.length} URLs.`);
